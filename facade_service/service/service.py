b6cd1e7d4eccbb976e18a101b22c9177412f042f
Bohdan <bogdan.magometa@gmail.com>
2023-05-29 00:56:03 +0300

    Refactor

--- facade_service/service/service.py
+++ facade_service/service/service.py
@@ -9,29 +9,27 @@ from hazelcast import HazelcastClient
 import time
 
 QUEUE_NAME = "messages_queue"
+HAZELCAST_NODE = "logging"
 
 class FacadeService:
     def __init__(self, logging_uri: str, messaging_uri: str):
         self._repository = FacadeRepository(logging_uri, messaging_uri)
 
-        print("Started connecting to hazelcast")
-        ts = time.time()
-        self._hazelcast_client = HazelcastClient(cluster_members=["logging"], connection_timeout=30.0)
-        print("Finished connecting. Time passed =", time.time() - ts, "seconds")
-        self._messages_queue = self._hazelcast_client.get_queue(QUEUE_NAME).blocking()
+        self._hazelcast_client = HazelcastClient(cluster_members=[HAZELCAST_NODE], connection_timeout=30.0)
+        self._messages_queue = self._hazelcast_client.get_queue(QUEUE_NAME)
 
     async def add_message(self, msg_text: str) -> bool:
         message = MessageFactory.build(message_text=msg_text)
         print("Received message " + message.json())
         queue_fut = self._messages_queue.put(message)
-        repository_fut = await self._repository.log_message(message)
-        # _, status = await asyncio.gather(queue_fut, repository_fut)
-        return repository_fut
+        repository_fut = self._repository.log_message(message)
+        _, status = await asyncio.gather(queue_fut, repository_fut)
+        return status
 
     async def read_messages(self) -> Optional[List[Message]]:
-        cor1 = await self._repository.read_logged_messages()
-        cor2 = await self._repository.read_messages()
-        logged_messages, messages = cor1, cor2 #await asyncio.gather(cor1, cor2)
+        cor1 = self._repository.read_logged_messages()
+        cor2 = self._repository.read_messages()
+        logged_messages, messages = await asyncio.gather(cor1, cor2)
 
         if logged_messages is None or messages is None:
             return None
--- messages_service/controller.py
+++ messages_service/controller.py
@@ -13,6 +13,7 @@ from domain import Message
 app = FastAPI()
 app.messages_service = MessagesService()
 QUEUE_NAME = "messages_queue"
+HAZELCAST_NODE = "logging"
 
 @app.get("/")
 async def get_messages() -> List[Message]:
@@ -20,19 +21,14 @@ async def get_messages() -> List[Message]:
     return messages
 
 def poll_messages_queue():
-    # print("Starting")
-    hazelcast_client = HazelcastClient(cluster_members=["logging"], connection_timeout=30.0)
+    hazelcast_client = HazelcastClient(cluster_members=[HAZELCAST_NODE], connection_timeout=30.0)
     messages_queue = hazelcast_client.get_queue(QUEUE_NAME)
     while True:
-        # import time
-        # time.sleep(1)
-        # print("Ay")
         message = messages_queue.take().result()
         app.messages_service.add_message(message)
 
 @app.on_event("startup")
 async def app_startup():
-    # asyncio.create_task(poll_messages_queue())
     asyncio.get_running_loop().run_in_executor(None, poll_messages_queue)
 
 if __name__ == "__main__":
